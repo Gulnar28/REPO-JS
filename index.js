158e402fadb6ba88fc7d25de04a50e085749b047
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-04-16 13:37:14 +0400

    commit

--- index.js
+++ index.js
@@ -11,69 +11,77 @@
 
 
 
-//  const printFullName = (firstName, lastName) => {
-//      return `${firstName} ${lastName}`;
-//    }
+
+
+  const printFullNamewithparam = (firstName, lastName) => {
+      return `${firstName} ${lastName}`;
+    }
 
   
-// //    console.log(printFullName('Gulnar', 'Mammadova'))
-
-
-//  function perimeterRectangle(a, b) {
-//     let perimeter = 2 * (a + b);
-//      return perimeter;
-//   }
-//  console.log(perimeterOfRectangle(3,4));
+    console.log(printFullNamewithparam('Gulnar', 'Mammadova'))
 
 
 
 
-//  function bodyMassIndex (){
-//      let weight =a
-//      let height = b
+
+  function perimeterRectangle(a, b) {
+     let perimeter = 2 * (a + b);
+     return perimeter;
+ }
+  console.log(perimeterOfRectangle(3,4));
+
+
+
+
+
+
+  function bodyMassIndex (weight,height){
+          let weight =a
+          let height = b
+        
+        return  weight/(height*height);
+      };
+      console.log(bodyMassIndex(weight,height));
     
-//     return  weight/(height*height);
-//  };
-//  console.log(bodyMassIndex(weight,height));
-
-//  let BMI = bodyMassIndex(weight,height);
-
-//  if(BMI<18.5){
-//      console.log("Underweight")}
-//  else if (BMI>=18.5 & BMI<=24.9){
-//      console.log("Normal Weight")
-//  }
-//  else if (BMI>=25 & BMI<=29.9){
-//      console.log("Normal Weight")
-//  }
-//  else  {
-//      console.log("Obese")
-//  }
+      let BMI = bodyMassIndex(weight,height);
+    
+      if(BMI<18.5){
+          console.log("Underweight")}
+      else if (BMI>=18.5 & BMI<=24.9){
+          console.log("Normal Weight")
+      }
+     else if (BMI>=25 & BMI<=29.9){
+          console.log("Normal Weight")
+      }
+      else  {
+          console.log("Obese")
+      }
 
 
 
-// function findMax(arr){
-//     let max=arr[0]
-//     let min=arr[0]
 
-//     for( let i=0;i< arr.length;i++){ 
-//         if (arr[i]>max){
-//             max=arr[i];
-//        }
 
-//  if (arr[i]<min){
+ function findMax(a,b,c){
 
-//      min=arr[i];
-//  }
-   
-//   Console.log("max "+max)
-// //  
-//  }
+ if (a>=b && a>=c){
+
+     Console.log("a maximumdur")
+ }
+else if (b>=a && b>=c){
+
+         Console.log("b maximumdur")
+     }
+     else{
+      Console.log( "c maximumdur")
+     }
+
+ 
+    }
   
 
-//  function pow(x, n) {
-//       let result = x**n;
-//       return result;
-// }
-//  console.log(pow(3,2))
+  function pow(x, n) {
+       let result = x**n;
+      return result;
+ }
+ console.log(pow(3,2))
 

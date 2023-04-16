fd6c4d836b511e40bc70dfcfc1e3ebeb14b70e3f
gulnarvma@outlook.com <gulnarvma@outlook.com>
2023-04-16 11:32:52 +0400

    commmit

--- index.js
+++ index.js
@@ -1,100 +1,79 @@
-// //function pow(x, n) {
-//     let result = x;
-  
-//     for (let i = 1; i < n; i++) {
-//       result *= x;
-//     }
-  
-//     return result;//
-
-
-1.
-// function printFullName (){
-//     let firstName = 'Gulnar'
-//     let lastName = 'Mammadova'
-//     
-//     let fullName = firstName  + lastName;
-//     return fullName;
-// }
-// console.log(printFullName());
-
-
-// function perimeterRectangle(a, b) {
-//     let perimeter = 2 * (a + b);
-//     return perimeter;
-//   }
-console.log(perimeterOfRectangle());
-const printFullName = (firstName, lastName) => {
-    return `${firstName} ${lastName}`
-  }
-  
-  console.log(printFullName('Asabeneh', 'Yetayeh'))
-
-  let weight =70
-let height = 174
-
-function bodyMassIndex (){
-    let weight =70
-    let height = 174
-    
-   return  (weight/((height/100)*(height/100)))
-};
-console.log(bodyMassIndex(weight,height));
-
-let BMI = bodyMassIndex(weight,height);
-
-if(BMI<18.5){
-    console.log("Underweight")}
-else if (BMI>=18.5 & BMI<=24.9){
-    console.log("Normal Weight")
-}
-else if (BMI>=25 & BMI<=29.9){
-    console.log("Normal Weight")
-}
-else  {
-    console.log("Obese")
-}
-
-
-
-function findMax(numbers){
-   let max=numbers[0]
-
-   for( let i=0;i< numbers.length;i++){ 
-       if (numbers[i]>max){
-           max=numbers[i];
-       }
-
-else if (numbers[i]<min){
-
-    min=numbers[i];
-}
+ function printFullName (){
+    let firstName = 'Gulnar'
+    let lastName = 'Mammadova'
    
-   Console.log("Max:${max}")
-}
+   let fullName = firstName  + lastName;
+  return fullName;
+ }
+ console.log(printFullName())
+
+
+
+
+
+//  const printFullName = (firstName, lastName) => {
+//      return `${firstName} ${lastName}`;
+//    }
+
+  
+// //    console.log(printFullName('Gulnar', 'Mammadova'))
+
+
+//  function perimeterRectangle(a, b) {
+//     let perimeter = 2 * (a + b);
+//      return perimeter;
+//   }
+//  console.log(perimeterOfRectangle(3,4));
+
+
+
+
+//  function bodyMassIndex (){
+//      let weight =a
+//      let height = b
+    
+//     return  weight/(height*height);
+//  };
+//  console.log(bodyMassIndex(weight,height));
+
+//  let BMI = bodyMassIndex(weight,height);
+
+//  if(BMI<18.5){
+//      console.log("Underweight")}
+//  else if (BMI>=18.5 & BMI<=24.9){
+//      console.log("Normal Weight")
+//  }
+//  else if (BMI>=25 & BMI<=29.9){
+//      console.log("Normal Weight")
+//  }
+//  else  {
+//      console.log("Obese")
+//  }
+
+
+
+// function findMax(arr){
+//     let max=arr[0]
+//     let min=arr[0]
+
+//     for( let i=0;i< arr.length;i++){ 
+//         if (arr[i]>max){
+//             max=arr[i];
+//        }
+
+//  if (arr[i]<min){
+
+//      min=arr[i];
+//  }
+   
+//   Console.log("max "+max)
+// //  
+//  }
   
 
+//  function pow(x, n) {
+//       let result = x**n;
+//       return result;
+// }
+//  console.log(pow(3,2))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
